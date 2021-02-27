32c712542bd4e8ac3fd9c5a23c814d4974d93356
Gregor Baues <gr2bba@gmail.com>
2021-02-27 21:56:41 +0100

    Release V1.0

--- src/src.ino
+++ src/src.ino
@@ -5,7 +5,7 @@ void setup() {
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
 }
-
+// v1.0
 // the loop function runs over and over again forever
 void loop() {
   digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
